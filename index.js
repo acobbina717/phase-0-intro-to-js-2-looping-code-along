b3af224a300124eda7cb757b572203d713e4e591
Abeku <acobbina717@gmail.com>
2022-04-05 11:12:02 -0400

    commit tree d7bb252079b31465eb324983bf7eb15bb3c814d3

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name) {
+    let arry = []
+    for (let x = 0; x < name.length; x++) {
+        arry.push(`Thank you, ${name[x]}, for the wonderful surprise gift!`)
+    }
+    return arry
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
